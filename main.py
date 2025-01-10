c86e2a3f1f6bb5017882ae20fdf9ce2bf8a4f7e3
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2025-01-10 22:59:25 +0100

    peg game to 60FPS

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def main():
   print(f"Screen height: {SCREEN_HEIGHT}")
 
   screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+  clock = pygame.time.Clock()
+  dt = 0
 
   while True:
     for event in pygame.event.get():
@@ -17,6 +19,8 @@ def main():
     black = (0,0,0)
     pygame.Surface.fill(screen, black)
     pygame.display.flip()
+    delta_time = clock.tick(60)
+    dt = delta_time / 1000 #convert milliseconds to seconds
 
 
 if __name__ == "__main__":
