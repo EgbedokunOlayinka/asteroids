bbbffb5c59eb0622fb4c2848b67b184920881224
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2025-01-10 23:29:34 +0100

    draw and update player on game screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
   pygame.init()
@@ -11,6 +12,10 @@ def main():
   clock = pygame.time.Clock()
   dt = 0
 
+  x = SCREEN_WIDTH / 2
+  y = SCREEN_HEIGHT / 2
+  player = Player(x, y)
+
   while True:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
@@ -18,10 +23,11 @@ def main():
       
     black = (0,0,0)
     pygame.Surface.fill(screen, black)
+    player.draw(screen)
     pygame.display.flip()
     delta_time = clock.tick(60)
     dt = delta_time / 1000 #convert milliseconds to seconds
-
+    player.update(dt)
 
 if __name__ == "__main__":
   main()
