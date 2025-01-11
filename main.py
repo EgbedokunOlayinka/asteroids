c0af873a0018a036d1510ca42fea569eb92d4d0c
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2025-01-11 20:07:51 +0100

    add some comments

--- main.py
+++ main.py
@@ -19,18 +19,27 @@ def main():
   Player.containers = (updatable, drawable)
   player = Player(x, y)
 
+  #start game loop
   while True:
+    #handle game quit
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         return
       
+    #run update for classes in updatable group
     for item in updatable:
       item.update(dt)
+
     black = (0,0,0)
     pygame.Surface.fill(screen, black)
+
+    #run draw for all classes in drawable group
     for item in drawable:
       item.draw(screen)
+
     pygame.display.flip()
+
+    #peg game to 60FPS
     delta_time = clock.tick(60)
     dt = delta_time / 1000 #convert milliseconds to seconds
 
