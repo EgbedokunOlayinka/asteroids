0f3a868bc44bab4ee0c9cffb7811c7929f7f40c8
EgbedokunOlayinka <egbedokunolayinka@gmail.com>
2025-01-11 20:30:11 +0100

    create and spawn asteroids on the game screen

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
   pygame.init()
@@ -12,12 +14,19 @@ def main():
   clock = pygame.time.Clock()
   dt = 0
 
-  x = SCREEN_WIDTH / 2
-  y = SCREEN_HEIGHT / 2
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
+
   Player.containers = (updatable, drawable)
+  Asteroid.containers = (asteroids, updatable, drawable)
+  AsteroidField.containers = (updatable)
+
+  x = SCREEN_WIDTH / 2
+  y = SCREEN_HEIGHT / 2
   player = Player(x, y)
+
+  asteroid_field = AsteroidField()
 
   #start game loop
   while True:
